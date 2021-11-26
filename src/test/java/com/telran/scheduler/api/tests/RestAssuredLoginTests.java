04de5cdb6ca84fb1d69ed60c4c3921bdb2a2f3af
Kate <levifin7@gmail.com>
2021-11-26 20:27:45 +0300

    Added changes in registrationSuccessTest

--- src/test/java/com/telran/scheduler/api/tests/RestAssuredLoginTests.java
+++ src/test/java/com/telran/scheduler/api/tests/RestAssuredLoginTests.java
@@ -8,6 +8,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestAssuredLoginTests {
 
@@ -65,15 +67,24 @@ public class RestAssuredLoginTests {
 				.password("World2021")
 				.build();
 
-		String message = given()
+//		String message = given()
+//				.contentType(ContentType.JSON)
+//				.body(requestDto)
+//				.when()
+//				.post("login")
+//				.then()
+//				.assertThat().statusCode(200)
+//				.extract().path("message");
+//		System.out.println(message);
+
+		given()
 				.contentType(ContentType.JSON)
 				.body(requestDto)
 				.when()
 				.post("login")
 				.then()
-				.assertThat().statusCode(200)
-				.extract().path("message");
-
-		System.out.println(message);
+				.statusCode(200)
+				.assertThat().body("status", containsString("Registration success"))
+				.body("registration", equalTo(true));
 	}
 }
